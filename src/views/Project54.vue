bbb0d00b0cc05aeecfc80f122f8021d433dd6482
junyong <junyong.zeng@rebilly.com>
2025-08-28 19:02:55 +0800

    project 54 cleanup

--- src/views/Project54.vue
+++ src/views/Project54.vue
@@ -29,29 +29,6 @@ async function switchCurrency(cur) {
   });
 }
 
-async function setup() {
-  // Call the server endpoint to create a deposit request
-  const response = await fetch(`${import.meta.env.VITE_API_URL}/rebilly/create-cashier-project-54`, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({currency: 'USD'}),
-  });
-  const data = await response.json();
-  const mountElement = document.querySelector('#deposit');
-  const cashierToken = data.token;
-  if (window.RebillyCashier) {
-    RebillyCashier.renderDeposit({
-      mountElement,
-      cashierToken,
-    });
-  } else {
-    console.error('RebillyCashier library not loaded');
-  }
-}
-
 onMounted(async () => {
   await switchCurrency('USD');
 })
